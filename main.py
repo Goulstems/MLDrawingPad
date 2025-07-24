191df24eabae47d428c8cc60c00c6d769fbbc71e
Josh Morvant <sk84life4evr@Gmail.com>
2025-07-24 14:22:15 -0700

    deletedinnerFolder

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ import atexit
 
 Brain.loadModel()
 
+print("Can recognize:")
+for i in Brain.model.keys():
+    print("- "+i)
+print("- - - - - - - - -")
+
 app = tk.Tk()                                   #Create new tkinter application
                                                 #Ensure app saves model when exited
 atexit.register(Brain.saveModel)
